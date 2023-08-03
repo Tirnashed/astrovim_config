3c8a03fd1cb9059224684cd7c392cce51a257507
Tirnashed <nguyen.minh_dang@proton.me>
2023-08-03 09:19:44 +0200

    Revert last commits

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,12 +1,6 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.markdown" },
-  { import = "astrocommunity.pack.bash" },
-  { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.yaml" },
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
