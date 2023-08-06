bb94ff7f329beba59d6c6eca52bbb69a1e9b86ef
Tirnashed <nguyen.minh_dang@proton.me>
2023-08-06 18:39:06 +0200

    Use an older version of debugpy because the newest one cant be installed

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,7 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   -- example of imporing a plugin, comment out to use it or add your own
