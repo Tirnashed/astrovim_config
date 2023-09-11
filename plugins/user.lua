c457626e787b6284e0a5e1ecffc16f6879949aa8
Dang Nguyen <nguyen.minh_dang@proton.me>
2023-09-11 06:27:25 +0200

    Added new plugin neoscopes

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,56 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "Tirnashed/neoscopes",
+    event = "VeryLazy",
+    name = "neoscopes",
+    config = function()
+      local scopes = require "neoscopes"
+      scopes.add_startup_scope()
+      scopes.setup {
+        scopes = {
+          {
+            name = "project 1",
+            dirs = {},
+            glob_pattern = {},
+            additional_args = {},
+          },
+          {
+            name = "project 2",
+            dirs = {},
+            glob_pattern = {},
+            additional_args = {},
+          },
+        },
+        add_dirs_to_all_scopes = {},
+      }
+    end,
+    keys = {
+      {
+        "<leader>ss",
+        mode = { "n" },
+        function() require("neoscopes").select() end,
+        desc = "Select Scope",
+      },
+      {
+        "<leader>sg",
+        mode = { "n" },
+        function() require("neoscopes").live_grep() end,
+        desc = "Live Grep in Scope",
+      },
+      {
+        "<leader>sf",
+        mode = { "n" },
+        function() require("neoscopes").find_files() end,
+        desc = "Find Files in Scope",
+      },
+      {
+        "<leader>sc",
+        mode = { "n" },
+        function() require("neoscopes").grep_string() end,
+        desc = "Grep String in Scope",
+      },
+    },
+  },
 }
